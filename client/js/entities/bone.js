b4d9e58205c6cd90e3e27dbb5539caa41e7e27da
brag00n <35399662+brag00n@users.noreply.github.com>
2021-06-27 15:53:17 +0200

    Fix: Bone should be centred into the cell

--- client/js/entities/bone.js
+++ client/js/entities/bone.js
@@ -3,7 +3,9 @@ import { TILE_SIZE } from '../utils/constants.js';
 export default class Bone extends Phaser.GameObjects.Sprite {
 
   constructor(game, col, row) {
-    super(game, (col * TILE_SIZE), (row * TILE_SIZE), 'bone_tileset');
+    super(game, col + TILE_SIZE / 2, row  + TILE_SIZE / 2, 'bone_tileset');
+    this.game=game;
+    this.game.add.existing(this);
   }
 
 }
